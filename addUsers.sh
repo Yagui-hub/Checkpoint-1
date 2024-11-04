6cf743b062335bdf9866606c827969d6383915c9
Yagui-hub <yaguiyolou1@gmail.com>
2024-11-04 22:26:35 +0100

    addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,20 +1,29 @@
+
 #!/bin/bash
 
 # Vérifie si un argument est passé
-if [ $# -eq 0 ];
-then
-  echo "Erreur : aucun nom d'utilisateur fourni."
-  echo "Utilisation : $0 user1 user2 user3"
-  exit 1
+if [ $# -eq 0 ]; then
+    echo "Erreur : aucun nom d'utilisateur fourni."
+    echo "Utilisation : $0 user1 user2 user3"
+    exit 1
 fi
 
 # Parcours de tous les arguments pour créer les utilisateurs
-for username in "$@";
-do
-  # Crée l'utilisateur
-  sudo useradd "$username"
-  echo "Utilisateur $username créé avec succès."
-  echo "Erreur lors de la création de l'utilisateur $username."
+for username in "$@"; do
+    # Vérification de l'existence de l'utilisateur
+    if id "$username" &>/dev/null; then
+        echo "L'utilisateur $username existe déjà."
+    else
+        # Crée l'utilisateur
+        sudo useradd "$username"
+        
+        # Vérification de la création de l'utilisateur
+        if id "$username" &>/dev/null; then
+            echo "Utilisateur $username créé avec succès."
+        else
+            echo "Erreur lors de la création de l'utilisateur $username."
+        fi
+    fi
 done
 
 exit 0
